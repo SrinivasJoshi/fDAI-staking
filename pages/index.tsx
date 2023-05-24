b1a14e9c611071f837eedea595a35b09dfc2f769
Srinivas Joshi <srinivasjoshi60@gmail.com>
2023-05-24 23:10:50 +0530

    basic UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
 import Head from 'next/head';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { SITE_NAME, SITE_DESCRIPTION, SITE_URL } from '@/utils/config';
 import { Inter } from 'next/font/google';
-import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,81 +10,146 @@ export default function Home() {
 		typeof window !== 'undefined' && window.location.origin
 			? window.location.origin
 			: SITE_URL;
-
-	const links = [
-		{
-			title: 'Next.js',
-			description:
-				'Seamlessly integrate your decentralized application with Next.js, a popular React-based framework.',
-			href: 'https://nextjs.org',
-		},
-		{
-			title: 'RainbowKit',
-			description: 'A powerful and easy-to-use wallet Ethereum-based dApps.',
-			href: 'https://www.rainbowkit.com',
-		},
-		{
-			title: 'WAGMI',
-			description:
-				'wagmi is a collection of React Hooks containing everything you need to start working with Ethereum.',
-			href: 'https://wagmi.sh',
-		},
-		{
-			title: 'Examples',
-			description:
-				'Start by exploring some pre-built examples to inspire your creativity!',
-			href: `${origin}/examples`,
-		},
-	];
 	return (
 		<main
-			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-		>
+			className={`flex min-h-screen flex-col items-center justify-around px-20 pt-10 ${inter.className}`}>
 			<Head>
 				<title>{SITE_NAME}</title>
 				<meta name='description' content={SITE_DESCRIPTION} />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
-			<div className='z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex'>
+			<nav className='z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex'>
 				<p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-					Get started by editing&nbsp;
-					<code className='font-mono font-bold'>pages/index.tsx</code>
+					fDAI Staking
 				</p>
 				<div className='bottom-0 left-0 flex items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black static h-auto w-auto lg:bg-none my-24 lg:my-0'>
 					<ConnectButton />
 				</div>
-			</div>
+			</nav>
+			<div className='flex flex-col items-center justify-evenly w-full max-w-7xl'>
+				<p className='w-1/2 my-5 text-lg text-center'>
+					Welcome to my staking dApp! Stake your fDAI and earn Radish Tokens
+					(RDT) as rewards. Take part in our innovative staking system to grow
+					your holdings and maximize your earnings. Join now and unlock the
+					power of decentralized finance!
+				</p>
+				<div className='flex justify-between w-4/5 items-center font-'>
+					<h2 className='text-xl'>Your fDAI balance : {1200}</h2>
+					<h2 className='text-xl'>Reward per token per day: {1}</h2>{' '}
+					<h2 className='text-xl'>Your RDT balance : {120}</h2>
+				</div>
 
-			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-				<Image
-					className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-					src='/boilr3.svg'
-					alt='BOILR3 Logo'
-					width={250}
-					height={37}
-					priority
-				/>
-			</div>
-			<div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left mt-24 lg:mt-0'>
-				{links.map((link, index) => (
-					<Link
-						key={index}
-						href={link.href}
-						className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-						rel='noopener noreferrer'
-					>
-						<h2 className={`mb-3 text-2xl font-semibold`}>
-							{link.title}
-							<span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-								-&gt;
-							</span>
-						</h2>
-						<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-							{link.description}
-						</p>
-					</Link>
-				))}
+				<div className='flex justify-evenly w-4/5 items-center'>
+					<form className='flex flex-col items-center border-2 border-gray-300 p-10 rounded-xl mt-10'>
+						<label
+							htmlFor='stake'
+							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+							Stake
+						</label>
+						<input
+							type='number'
+							id='stake'
+							aria-describedby='helper-text-explanation'
+							className='my-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+							placeholder='100'
+						/>
+						<button
+							type='submit'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							Submit
+						</button>
+					</form>
+
+					<form className='flex flex-col items-center border-2 border-gray-300 p-10 rounded-xl mt-10'>
+						<label
+							htmlFor='Unstake'
+							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+							Unstake
+						</label>
+						<input
+							type='number'
+							id='Unstake'
+							aria-describedby='helper-text-explanation'
+							className='my-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+							placeholder='100'
+						/>
+						<button
+							type='submit'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							Submit
+						</button>
+					</form>
+				</div>
+
+				<div className='flex justify-evenly w-4/5 items-center'>
+					<form className='flex flex-col items-center border-2 border-gray-300 p-8 rounded-xl mt-10'>
+						<label
+							htmlFor='Withdraw'
+							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+							Withdraw Yield
+						</label>
+
+						<button
+							type='submit'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							Submit
+						</button>
+					</form>
+					<form className='flex flex-col items-center border-2 border-gray-300 p-8 rounded-xl mt-10'>
+						<label
+							htmlFor='Yield'
+							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+							Calculate Yield
+						</label>
+						<button
+							type='submit'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							Submit
+						</button>
+					</form>
+					<form className='flex flex-col items-center border-2 border-gray-300 p-8 rounded-xl mt-10'>
+						<label
+							htmlFor='mint'
+							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+							Mint fDAI
+						</label>
+						<button
+							type='submit'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							Submit
+						</button>
+					</form>
+					{/* <form className='border-2 border-gray-300 p-10 rounded-xl mt-10'>
+						<label
+							htmlFor='Unstake'
+							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+							Unstake
+						</label>
+						<input
+							type='number'
+							id='Unstake'
+							aria-describedby='helper-text-explanation'
+							className='my-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+							placeholder='100'
+						/>
+						<button
+							type='submit'
+							className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							Submit
+						</button>
+					</form> */}
+				</div>
 			</div>
 		</main>
 	);
 }
+
+// 1. Stake form
+// 1. Unstake form
+// 1. Calculate yield
+// 1. Earn yield
+// 1. Show fDAI balance
+// 1. mint fDAI here
+
+// -write all functions
+// -make componetns for those 4
--- utils/config.ts
+++ utils/config.ts
@@ -1,11 +1,10 @@
-import { mainnet, polygon, sepolia, polygonMumbai } from 'wagmi/chains';
+import { polygonMumbai } from 'wagmi/chains';
 
-export const ETH_CHAINS = [mainnet, polygon, sepolia, polygonMumbai];
+export const ETH_CHAINS = [polygonMumbai];
 export const WALLET_CONNECT_PROJECT_ID = 'YOUR_WALLET_CONNECT_PROJECT_ID';
 
-export const SITE_NAME = 'boilr3';
-export const SITE_DESCRIPTION =
-	'The Ultimate Next.js dApp Boilerplate with RainbowKit, Tailwind CSS & WAGMI';
+export const SITE_NAME = 'DAI-staking';
+export const SITE_DESCRIPTION = 'Stake Fake DAI and earn Reward Token : RDT';
 export const SITE_URL = 'https://boilr3.vercel.app';
 
 export const SOCIAL_TWITTER = 'Envoy_1084';
@@ -13,6 +12,770 @@ export const SOCIAL_GITHUB = 'Envoy-VC/boilr3';
 
 export const NFT_CONTRACT_ADDRESS =
 	'0x0Fc5f8A784810dEd101BD734cC59F6F7b868A3AF';
+
+export const FDAI_ADDRESS = '0x15F0Ca26781C3852f8166eD2ebce5D18265cceb7';
+export const STAKE_CONTRACT_ADDRESS =
+	'0x7210044D732F7f25F11Cc41c00e8B091F9d6a0C1';
+export const STAKE_CONTRACT_ABI = [
+	{
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '_rewardRate',
+				type: 'uint256',
+			},
+			{
+				internalType: 'address',
+				name: 'daiAddress',
+				type: 'address',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'owner',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'value',
+				type: 'uint256',
+			},
+		],
+		name: 'Approval',
+		type: 'event',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'approve',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'subtractedValue',
+				type: 'uint256',
+			},
+		],
+		name: 'decreaseAllowance',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'addedValue',
+				type: 'uint256',
+			},
+		],
+		name: 'increaseAllowance',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '_amount',
+				type: 'uint256',
+			},
+		],
+		name: 'stake',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'staker',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'amountStaked',
+				type: 'uint256',
+			},
+		],
+		name: 'Stake',
+		type: 'event',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'transfer',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'value',
+				type: 'uint256',
+			},
+		],
+		name: 'Transfer',
+		type: 'event',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'transferFrom',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '_amount',
+				type: 'uint256',
+			},
+		],
+		name: 'unstake',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'staker',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'amountStaked',
+				type: 'uint256',
+			},
+		],
+		name: 'Unstake',
+		type: 'event',
+	},
+	{
+		inputs: [],
+		name: 'withdrawYield',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'staker',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'YieldCollected',
+				type: 'uint256',
+			},
+		],
+		name: 'WithdrawYield',
+		type: 'event',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'owner',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+		],
+		name: 'allowance',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'account',
+				type: 'address',
+			},
+		],
+		name: 'balanceOf',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+		],
+		name: 'calculateYield',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+		],
+		name: 'calculateYieldTime',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'daiToken',
+		outputs: [
+			{
+				internalType: 'contract IERC20',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'decimals',
+		outputs: [
+			{
+				internalType: 'uint8',
+				name: '',
+				type: 'uint8',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'name',
+		outputs: [
+			{
+				internalType: 'string',
+				name: '',
+				type: 'string',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'rewardRate',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'symbol',
+		outputs: [
+			{
+				internalType: 'string',
+				name: '',
+				type: 'string',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'totalSupply',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+];
+
+export const FDAI_CONTRACT_ABI = [
+	{
+		inputs: [
+			{
+				internalType: 'string',
+				name: 'name',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: 'symbol',
+				type: 'string',
+			},
+			{
+				internalType: 'uint8',
+				name: 'decimals',
+				type: 'uint8',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'owner',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'value',
+				type: 'uint256',
+			},
+		],
+		name: 'Approval',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'value',
+				type: 'uint256',
+			},
+		],
+		name: 'Transfer',
+		type: 'event',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'owner',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+		],
+		name: 'allowance',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'approve',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'account',
+				type: 'address',
+			},
+		],
+		name: 'balanceOf',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'decimals',
+		outputs: [
+			{
+				internalType: 'uint8',
+				name: '',
+				type: 'uint8',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'subtractedValue',
+				type: 'uint256',
+			},
+		],
+		name: 'decreaseAllowance',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'addedValue',
+				type: 'uint256',
+			},
+		],
+		name: 'increaseAllowance',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'account',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'mint',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'name',
+		outputs: [
+			{
+				internalType: 'string',
+				name: '',
+				type: 'string',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'symbol',
+		outputs: [
+			{
+				internalType: 'string',
+				name: '',
+				type: 'string',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'totalSupply',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'transfer',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'from',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'to',
+				type: 'address',
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'transferFrom',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+];
 
 export const ironOptions = {
 	cookieName: SITE_NAME,
